cde512773593bc53e5c7f5d7e85ffb95092ddbe9
LueKely <luekely43110@gmail.com>
2022-09-28 13:07:57 +0800

    trying to figure out how emets work

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1 @click="alertUser">This is an about page</h1>
+		<p>gamer {{ props.joe }}</p>
+		{{ props.mama }}
+	</div>
 </template>
 
+<script setup>
+	const props = defineProps({ joe: Number, mama: Number });
+	const emit = defineEmits(['actionCaptured']);
+
+	function alertUser() {
+		emit('actionCaptured');
+	}
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+	@media (min-width: 1024px) {
+		.about {
+			min-height: 100vh;
+			display: flex;
+			align-items: center;
+		}
+	}
 </style>
