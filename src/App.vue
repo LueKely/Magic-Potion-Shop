46b081dd53c7421362fcaa910b4013a328e23c15
LueKely <luekely43110@gmail.com>
2022-10-02 14:56:44 +0800

    forgot to push ehe uwu fortnite battlepass

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <script setup>
 	import FormInput from './components/FormInput.vue';
 	import InputItems from './components/InputItems.vue';
-	import { ref } from 'vue';
+	import { ref, onMounted } from 'vue';
+
+	const itemArray = ref([]);
+
+	const isEmpty = localStorage.length == 0;
+
+	function pushArray(e) {
+		itemArray.value.push(e);
+	}
+
 	function submitToStorage(e) {
 		localStorage.setItem(
 			localStorage.length == 0 ? '1' : (localStorage.length + 1).toString(),
 			JSON.stringify(e)
 		);
+		pushArray(e);
 		console.log(localStorage);
 	}
-	const apple = ref(localStorage.getItem('1'));
+
+	function check() {
+		if (isEmpty) {
+			pushAll();
+		} else {
+			itemArray.value = [];
+		}
+	}
+
+	function pushAll() {
+		for (let index = 0; index < localStorage.length; index++) {
+			pushArray(JSON.parse(localStorage.getItem((index + 1).toString())));
+		}
+	}
+
+	onMounted(() => {
+		check();
+	});
 </script>
 
 <template>
 	<FormInput @item-out="submitToStorage"></FormInput>
-	<InputItems :item="apple"></InputItems>
+
+	<InputItems v-if="!isEmpty" :item="prop"></InputItems>
 </template>
 
 <style scoped></style>
--- src/components/InputItems.vue
+++ src/components/InputItems.vue
@@ -1,5 +1,16 @@
 <template>
-	<h1>{{ prop.item }}</h1>
+	<!-- parseItem image -->
+	<img :src="parseItem.url" :alt="parseItem.item" />
+	<!--parseItem tag -->
+	<div>
+		<p>{{ parseItem.tag }}</p>
+	</div>
+	<!-- parseItem name -->
+	<h2>{{ parseItem.label }}</h2>
+	<!-- parseItem price -->
+	<h3>
+		{{ parseItem.price }}
+	</h3>
 </template>
 <script setup>
 	import { reactive } from 'vue';
@@ -7,6 +18,4 @@
 	const prop = defineProps({ item: String });
 
 	const parseItem = reactive(JSON.parse(prop.item));
-
-	console.log(parseItem.tag);
 </script>
