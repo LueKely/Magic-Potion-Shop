44eda80907159f21c923aafdab6fadb8d490f132
Anunciacion Lue <luekely43110@gmail.com>
2022-10-03 22:18:35 +0800

    added the function to delete and emit

--- To do.md
+++ To do.md
@@ -2,6 +2,14 @@
 
 ## CURRENT
 
+URGENT
+lagyan mo ng template ref ung textarea back cancel ng button
+
+-make edit and delete button
+i can get there by removing the item in locale storage and the input component emiting the index and the editing to the app and splicing it choose to or edit you get the idea
+
+- make editing for for input item
+
 -make item card
 -form
 
--- src/components/InputItems.vue
+++ src/components/InputItems.vue
@@ -1,21 +1,62 @@
 <template>
 	<!-- container -->
 	<div>
-		<!-- prop.item image -->
-		<img :src="prop.item.url" :alt="prop.item.item" />
-		<!--prop.item tag -->
-		<div>
-			<p>{{ prop.item.tag }}</p>
+		<div v-show="!isEdit">
+			<!-- prop.item image -->
+			<img :src="prop.item.url" :alt="prop.item.url" />
+			<!--prop.item tag -->
+			<div>
+				<p>{{ prop.item.tag }}</p>
+			</div>
+
+			<!-- prop.item name -->
+			<h2>{{ prop.item.label }}</h2>
+			<!-- parseItem price -->
+			<h3>
+				{{ prop.item.price }}
+			</h3>
 		</div>
-		<!-- prop.item name -->
-		<h2>{{ prop.item.label }}</h2>
-		<!-- parseItem price -->
-		<h3>
-			{{ prop.item.price }}
-		</h3>
+		<div v-if="isEdit">
+			<textarea v-model="copyItem.url"></textarea>
+			<textarea v-model="copyItem.tag"></textarea>
+			<textarea v-model="copyItem.label"></textarea>
+			<textarea v-model="copyItem.price"></textarea>
+			<div>
+				<button @click="emitEdit">Submit</button
+				><button @click="makeFalse">cancel</button>
+			</div>
+		</div>
+
+		<div v-show="!isEdit">
+			<button @click="makeTrue">Edit</button>
+		</div>
+
+		<button @click="deleteItem">delete</button>
 	</div>
 </template>
 <script setup>
+	import { ref } from 'vue';
+	const isEdit = ref(false);
 	const prop = defineProps({ item: Object, index: Number });
-	console.log(prop.index);
+	const emit = defineEmits(['edited-item', 'deleted-item']);
+	const copyItem = ref({ ...prop.item });
+	const index = ref(prop.index);
+
+	function emitEdit() {
+		emit('edited-item', copyItem.value);
+		alert('your edit has been submitted');
+	}
+
+	function deleteItem() {
+		emit('deleted-item', index.value);
+		alert('item has now been deleted');
+	}
+
+	function makeTrue() {
+		isEdit.value = true;
+	}
+
+	function makeFalse() {
+		isEdit.value = false;
+	}
 </script>
